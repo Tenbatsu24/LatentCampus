42914dffe07875b1cdcd80bf57bd4e29b3bd0a54
Tenbatsu24 <puruvaish24@outlook.com>
2025-07-21 19:17:40 +0200

    change: weights in loss

--- src/nnssl/training/nnsslTrainer/aligned_mae/aligned_mae_eva_trainer.py
+++ src/nnssl/training/nnsslTrainer/aligned_mae/aligned_mae_eva_trainer.py
@@ -346,7 +346,7 @@ class AlignedMAEGImageFTEvaTrainer(AlignedMAEEvaTrainer):
         from nnssl.training.loss.aligned_mae_loss import AlignedMAELoss
 
         return AlignedMAELoss(
-            device=self.device, recon_weight=5.0, fg_cos_weight=1.0, ntxent_weight=1.0
+            device=self.device, recon_weight=5.0
         )
 
 
