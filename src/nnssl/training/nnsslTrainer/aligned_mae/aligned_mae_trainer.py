1132529de8bc767fd90f66e8383d9a5a56b5c353
Tenbatsu24 <puruvaish24@outlook.com>
2025-07-22 08:56:31 +0200

    add: no con trainer

--- src/nnssl/training/nnsslTrainer/aligned_mae/aligned_mae_trainer.py
+++ src/nnssl/training/nnsslTrainer/aligned_mae/aligned_mae_trainer.py
@@ -392,6 +392,20 @@ class AlignedMAEFTLowConTrainer(AlignedMAEFTTrainer):
         )
 
 
+class AlignedMAEFTNoConTrainer(AlignedMAEFTTrainer):
+
+    def build_loss(self):
+        """
+        Builds the loss function for the model.
+        This method is overridden to provide specific loss logic for low contrast ConMAE.
+        """
+        from nnssl.training.loss.aligned_mae_loss import AlignedMAELoss
+
+        return AlignedMAELoss(
+            device=self.device, recon_weight=5.0, fg_cos_weight=1.0, ntxent_weight=0.0
+        )
+
+
 class ConMAEFTTrainer(AlignedMAEFTTrainer):
 
     def build_loss(self):
